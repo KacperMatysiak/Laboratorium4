9591e12c2518dae72a431ddd075e2016dcc85093
KacperMatysiak <35163@studentwsei.pl>
2021-05-27 22:37:49 +0200

    LAB 4b 5.a

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -4,14 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
     public Label lbFile;
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
+    public ImageView imgMiniaturka;
+    public Button btnPokazInfografike;
 
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter( "Pliki XML (*.xml", "*.xml");
