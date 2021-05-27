6c0d22531714d8cfd80b81354e79663715a35ffa
KacperMatysiak <35163@studentwsei.pl>
2021-05-27 17:50:55 +0200

    zad 8 koniec

--- src/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
+++ src/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
@@ -5,14 +5,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TestFileRead {
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) throws IOException {
         try {
-            String contents = new String (Files.readAllBytes(Paths.get("gusInfoGraphic.xml")));
-            System.out.println("Zawartość pliku.xml");
-            System.out.print(contents);
+            String contents = new String(Files.readAllBytes(Paths.get("C:\\Users\\pulaw\\IdeaProjects\\Laboratorium4a\\src\\pl\\lublin\\wsei\\java\\cwiczenia\\gusInfoGraphic.xml")));
+            System.out.println("Zawartość pliku.xml: ");
+            System.out.println(contents);
         }
 
-        catch (IOException e) {
+         catch (IOException e) {
             System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml => "+ e.getLocalizedMessage());
             e.printStackTrace();
         }
