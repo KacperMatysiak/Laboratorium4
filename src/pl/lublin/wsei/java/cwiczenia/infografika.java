2eb5bb2038d0ad5f1d1c82ca3df8a6d3f47b6f9c
KacperMatysiak <35163@studentwsei.pl>
2021-05-27 19:18:01 +0200

    11d

--- src/pl/lublin/wsei/java/cwiczenia/infografika.java
+++ src/pl/lublin/wsei/java/cwiczenia/infografika.java
@@ -1,13 +1,60 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class infografika {
     String tytul;
     String adresStrony;
+    int szerokosc;
+    int wysokosc;
     String adresGrafiki;
     String adresMiniaturki;
-    String wysokosc;
-    String szerokosc;
-    public infografika(String tekst){
+
+    public infografika(String tekst) {
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(tekst);
+        if (m.find())
+            tytul = m.group(1);
+        else
+            tytul = "";
+
+        pat = Pattern.compile("<link>(.*)<\\/link>");
+        m = pat.matcher(tekst);
+        if (m.find())
+            adresStrony = m.group(1);
+        else
+            adresStrony = "";
+
+
+        pat = Pattern.compile("width=\"(.*)\"height");
+        m = pat.matcher(tekst);
+        if (m.find())
+            szerokosc = Integer.parseInt(m.group(1));
+        else
+            szerokosc = 0;
+
+        pat = Pattern.compile("height=\"(.*)\"\\/>");
+        m = pat.matcher(tekst);
+        if (m.find())
+            wysokosc = Integer.parseInt(m.group(1));
+        else
+            wysokosc = 0;
+
+        pat = Pattern.compile("<media:content url=\"(.*)\"");
+        m = pat.matcher(tekst);
+        if (m.find())
+            adresGrafiki = m.group(1);
+        else
+            adresGrafiki = "";
+
+        pat = Pattern.compile("<description><!\\[CDATA\\[<div><img src=\"(.*)\" a");
+        m = pat.matcher(tekst);
+        if (m.find())
+            adresMiniaturki = m.group(1);
+        else
+            adresMiniaturki = "";
+
 
     }
 }
